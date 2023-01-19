4e556078a59723e3fb1d2bcf565b206f85575275
devTatendaMarvelous <marveloustchimusoro@gmail.com>
2023-01-19 16:55:14 +0200

    initial commit

---
+++ products.js
@@ -0,0 +1,51 @@
+// DUMMY PRODUCTS (PRODUCT ID : DATA)
+var products = {
+  123: {
+    name : "MokBook Thicc",
+    desc : "Greatest properly off ham exercise all.",
+    img : "dummy-pdt-b.jpg",
+    price : 2034
+  },
+  124: {
+    name : "MokBook Rookie",
+    desc : "Unsatiable its possession nor off.",
+    img : "dummy-pdt-b.jpg",
+    price : 1247
+  },
+  125: {
+    name : "iPong Max",
+    desc : "All difficulty unreserved the solicitude.",
+    img : "dummy-pdt-a.jpg",
+    price : 675
+  },
+  126: {
+    name : "iTab Pok",
+    desc : "Had judgment out property the supplied. ",
+    img : "dummy-pdt-a.jpg",
+    price : 842
+  },
+  127: {
+    name : "TAB 6",
+    desc : "Had judgment out property the supplied. ",
+    img : "dummy-pdt-a.jpg",
+    price : 800
+  },
+  128: {
+    name : "TAB 20",
+    desc : "Had judgment out property the supplied. ",
+    img : "dummy-pdt-a.jpg",
+    price : 700
+  },
+  129: {
+    name : "TAB345",
+    desc : "Had judgment out property the supplied. ",
+    img : "dummy-pdt-a.jpg",
+    price : 600
+  },
+  130: {
+    name : "ITEL",
+    desc : "Had judgment out property the supplied. ",
+    img : "dummy-pdt-a.jpg",
+    price : 500
+  }
+};
